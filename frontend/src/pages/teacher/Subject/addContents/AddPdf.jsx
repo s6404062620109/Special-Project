c8b50c17b88ffe10dea6c34ff479b12ef8e7a055
Anuphab Khanchok <139010386+s6404062620109@users.noreply.github.com>
2025-04-18 17:07:29 +0700

    Update AddPdf.jsx

--- frontend/src/pages/teacher/Subject/addContents/AddPdf.jsx
+++ frontend/src/pages/teacher/Subject/addContents/AddPdf.jsx
@@ -1,9 +1,125 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { Box, Button, Stack, Typography, styled } from "@mui/material";
+import PreviewIcon from '@mui/icons-material/Preview';
+import AddIcon from '@mui/icons-material/Add';
 
-function AddPdf() {
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
+function AddPdf({
+  handlePreview,
+}) {
+  const inputRef = useRef(null);
+  const [ file, setFile ] = useState(null);
+
+  const handleBoxClick = () => {
+    inputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    if (selectedFile && selectedFile.type === "application/pdf") {
+      setFile(selectedFile);
+    }
+  };
+
   return (
-    <div>AddPdf</div>
-  )
+    <Stack
+      justifyContent='space-around'
+      alignItems='center'
+      gap={2}
+      sx={{
+        width: "100%",
+        marginTop: "20px"
+      }}
+    >
+      <Stack
+        sx={{
+          backgroundColor: 'white',
+          padding: '20px',
+          borderRadius: '8px',
+          width: { xs: '90%', sm: '50%' }
+        }}
+        gap={2}
+      >
+        <Stack 
+          direction="row" 
+          justifyContent="space-between" 
+          alignItems="center"
+        >
+          <Typography variant="h5" fontWeight="bold">
+            Subject
+          </Typography>
+          <Button
+            variant="outlined"
+            startIcon={<PreviewIcon />}
+            onClick={handlePreview}
+          >
+            Preview
+          </Button>
+        </Stack>
+
+        <Box
+          onClick={handleBoxClick}
+          sx={{
+            width: '100%',
+            height: '200px', 
+            border: '1px dashed #b3b3b3',
+            borderRadius: '8px',
+            position: 'relative',
+            '&:hover': {
+              borderColor: '#888',
+              background: '#f0f0f0',
+              cursor: 'pointer'
+            }
+          }}
+        >
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+            }}
+          >
+            <AddIcon
+              sx={{
+                color: '#b3b3b3'
+              }}
+            />
+            <Typography variant="h6" sx={{ color: '#b3b3b3' }}>Upload PDf here.</Typography>
+            {file && (
+              <Typography variant="body2" sx={{ color: '#666', mt: 1 }}>
+                Selected file: {file[0].name}
+              </Typography>
+            )}
+          </Stack>
+          
+          
+          <VisuallyHiddenInput
+            type="file"
+            ref={inputRef}
+            onChange={handleFileChange}
+            multiple
+            accept="application/pdf"
+          />
+        </Box>
+      </Stack>
+      
+    </Stack>
+  );
 }
 
-export default AddPdf
+export default AddPdf;
