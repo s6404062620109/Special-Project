6285d4bb91d5132b040480370a0d803f742f682f
Anuphab Khanchok <139010386+s6404062620109@users.noreply.github.com>
2025-08-06 18:43:52 +0700

    Reduce linux-terminal service .

--- backend/controller/labState.js
+++ backend/controller/labState.js
@@ -6,27 +6,27 @@ const labSessions = [
     userId: null,
     timeout: null,
   },
-  {
-    port: 4002,
-    container: "linux-terminal-user2",
-    inUse: false,
-    userId: null,
-    timeout: null,
-  },
-  {
-    port: 4003,
-    container: "linux-terminal-user3",
-    inUse: false,
-    userId: null,
-    timeout: null,
-  },
-  {
-    port: 4004,
-    container: "linux-terminal-user4",
-    inUse: false,
-    userId: null,
-    timeout: null,
-  },
+  // {
+  //   port: 4002,
+  //   container: "linux-terminal-user2",
+  //   inUse: false,
+  //   userId: null,
+  //   timeout: null,
+  // },
+  // {
+  //   port: 4003,
+  //   container: "linux-terminal-user3",
+  //   inUse: false,
+  //   userId: null,
+  //   timeout: null,
+  // },
+  // {
+  //   port: 4004,
+  //   container: "linux-terminal-user4",
+  //   inUse: false,
+  //   userId: null,
+  //   timeout: null,
+  // },
 ];
 
 module.exports = labSessions;
