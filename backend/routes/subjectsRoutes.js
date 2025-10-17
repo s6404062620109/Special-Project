59113f59c7ac107e6f227ee46d1f826913a69d43
Anuphab Khanchok <139010386+s6404062620109@users.noreply.github.com>
2025-10-18 01:32:48 +0700

    Update subjectsRoutes.js

--- backend/routes/subjectsRoutes.js
+++ backend/routes/subjectsRoutes.js
@@ -9,7 +9,7 @@ router.use('/courses', express.static(path.join(__dirname, '../courses')));
 
 router.get("/getAllSubject/:courseId", authUserRole.checkCourseCreation, subjectController.getAll);
 
-router.get("/getAllSubjectStudent/:courseId", authUserRole.checkStudentRole, subjectController.getAllSubjectStudent);
+router.get("/getAllSubjectStudent/:courseId", subjectController.getAllSubjectStudent);
 
 router.get("/getSubject/:courseId/:subjectId", authUserRole.verifiedStudentEnrollCourse, subjectController.getSubject);
 
