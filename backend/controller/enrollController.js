c531607c4f8aba45c85db94115286198ba58069d
Anuphab Khanchok <139010386+s6404062620109@users.noreply.github.com>
2025-10-25 19:33:27 +0700

    Update enrollController.js

--- backend/controller/enrollController.js
+++ backend/controller/enrollController.js
@@ -155,7 +155,7 @@ const checkCourseEnroll = (req, res) => {
     }
 
     try{
-      db.query(`SELECT * FROM enrollment WHERE id = ? AND userId = ? AND courseId = ? AND posttest_complete IN (0, -1) AND pretest_complete IN (0, 1)`, 
+      db.query(`SELECT * FROM enrollment WHERE id = ? AND userId = ? AND courseId = ? AND posttest_complete IN (0, 1, -1) AND pretest_complete IN (0, 1)`, 
         [enrollmentId, userId, courseId], (err, results) => {
         if (err) {
           console.error(err);
