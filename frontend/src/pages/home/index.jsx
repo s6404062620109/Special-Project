d979980a446eecbd90ef21e7f62fdacb71f9f2af
Anuphab Khanchok <139010386+s6404062620109@users.noreply.github.com>
2025-11-03 15:50:19 +0700

    Deploy final.

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -170,7 +170,7 @@ function Home() {
                   borderRadius: 4,
                   boxShadow: '4px 4px 6px rgba(0,0,0,0.08)',
                   mb: 4,
-                  width: { xs: "100%", md: "90%" },
+                  width: { xs: "100%", md: "80%" },
                   margin: "0 auto"
                 }}
               >
